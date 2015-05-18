90aaa04b6913b375f9dcc217c35ff49963b3c6dc
root <root@vagrant-centos65.vagrantup.com>
2015-05-18 08:32:38 +0000

    4:ビューへ値を渡す

--- server_app/app/Http/Controllers/PagesController.php
+++ server_app/app/Http/Controllers/PagesController.php
@@ -8,6 +8,19 @@ use Illuminate\Http\Request;
 class PagesController extends Controller {
 
 	public function about() {
-		return view('pages.about');
+		// 配列に値をセット
+		$data = [];
+		$data["first_name"] = "Luke";
+		$data["last_name"] = "Skywalker";
+
+        // view関数の第２引数に配列を渡す
+		return view('pages.about', $data);
+		// return view('pages.about');
+        // 変数に値をセット
+		// $first_name = "Luke";
+		// $last_name = "Skywalker";
+
+  //       // view関数の第２引数に compact関数を使う
+		// return view('pages.about', compact('first_name',
 	}
 }
