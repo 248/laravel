b642bf4c0c250327e086753d426e7f0fdc25f5c3
root <root@vagrant-centos65.vagrantup.com>
2015-05-19 05:14:28 +0000

    15:BOOTSTRAP3の導入

--- server_app/resources/views/layout.blade.php
+++ server_app/resources/views/layout.blade.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>My Blog</title>
+
+    <!-- CSS --><!-- 追加 -->
+    <link href="/css/app.css" rel="stylesheet">
 </head>
+
 <body>
+    <div class="container"><!-- 追加 -->
+        @yield('content')
+    </div>
 
-    @yield('content')
-
+    <!-- Scripts --><!-- 追加 -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
