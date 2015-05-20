58e1b4d5c21ed0a44590fd3b3634e42cb89a2831
root <root@vagrant-centos65.vagrantup.com>
2015-05-20 01:29:51 +0000

    26:ナビゲーションヘッダー

--- server_app/app/Http/routes.php
+++ server_app/app/Http/routes.php
@@ -17,6 +17,7 @@ Route::get('contact', 'WelcomeController@contact');
 
 Route::get('about', 'PagesController@about');
 
+Route::get('/', 'ArticlesController@index');  // root を記事一覧にします
 Route::resource('articles', 'ArticlesController');
 
 // Route::get('articles', 'ArticlesController@index');
--- server_app/resources/views/layout.blade.php
+++ server_app/resources/views/layout.blade.php
@@ -1,26 +1,28 @@
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<title>My Blog</title>
+    <meta charset="UTF-8">
+    <title>My Blog</title>
 
-	<!-- CSS --><!-- 追加 -->
-	<link href="/css/app.css" rel="stylesheet">
+    <!-- CSS --><!-- 追加 -->
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 
 <body>
-	<div class="container"><!-- 追加 -->
-		{{-- フラッシュメッセージの表示 --}}
-		@if (Session::has('flash_message'))
-		<div class="alert alert-success">{{ Session::get('flash_message') }}</div>
-		@endif
+    {{-- ナビゲーションバーの Partial を使用 --}}
+    @include('navbar')
+    <div class="container"><!-- 追加 -->
+        {{-- フラッシュメッセージの表示 --}}
+        @if (Session::has('flash_message'))
+        <div class="alert alert-success">{{ Session::get('flash_message') }}</div>
+        @endif
 
-		{{-- コンテンツの表示 --}}
-		@yield('content')
-	</div>
+        {{-- コンテンツの表示 --}}
+        @yield('content')
+    </div>
 
-	<!-- Scripts --><!-- 追加 -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <!-- Scripts --><!-- 追加 -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
