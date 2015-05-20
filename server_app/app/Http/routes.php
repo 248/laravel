11926f05074da0fb73818a54cb477ec5426d2306
root <root@vagrant-centos65.vagrantup.com>
2015-05-20 01:23:54 +0000

    25:RESTFUL RESOUCE CONTROLLER

--- server_app/app/Http/routes.php
+++ server_app/app/Http/routes.php
@@ -17,6 +17,8 @@ Route::get('contact', 'WelcomeController@contact');
 
 Route::get('about', 'PagesController@about');
 
+Route::resource('articles', 'ArticlesController');
+
 // Route::get('articles', 'ArticlesController@index');
 // Route::get('articles/create', 'ArticlesController@create'); // ルートは記述順にマッチングされるため
 // Route::get('articles/{id}', 'ArticlesController@show');
@@ -25,13 +27,13 @@ Route::get('about', 'PagesController@about');
 // Route::patch('articles/{id}', 'ArticlesController@update');
 // Route::delete('articles/{id}', 'ArticlesController@destroy');
 
-Route::get('articles', ['as' => 'articles.index', 'uses' => 'ArticlesController@index']);
-Route::get('articles/create', ['as' => 'articles.create', 'uses' => 'ArticlesController@create']);
-Route::get('articles/{id}', ['as' => 'articles.show', 'uses' => 'ArticlesController@show']);
-Route::post('articles', ['as' => 'articles.store', 'uses' => 'ArticlesController@store']);
-Route::get('articles/{id}/edit', ['as' => 'articles.edit', 'uses' => 'ArticlesController@edit']);
-Route::patch('articles/{id}', ['as' => 'articles.update', 'uses' => 'ArticlesController@update']);
-Route::delete('articles/{id}', ['as' => 'articles.destroy', 'uses' => 'ArticlesController@destroy']);
+// Route::get('articles', ['as' => 'articles.index', 'uses' => 'ArticlesController@index']);
+// Route::get('articles/create', ['as' => 'articles.create', 'uses' => 'ArticlesController@create']);
+// Route::get('articles/{id}', ['as' => 'articles.show', 'uses' => 'ArticlesController@show']);
+// Route::post('articles', ['as' => 'articles.store', 'uses' => 'ArticlesController@store']);
+// Route::get('articles/{id}/edit', ['as' => 'articles.edit', 'uses' => 'ArticlesController@edit']);
+// Route::patch('articles/{id}', ['as' => 'articles.update', 'uses' => 'ArticlesController@update']);
+// Route::delete('articles/{id}', ['as' => 'articles.destroy', 'uses' => 'ArticlesController@destroy']);
 
 // Route::get('home', 'HomeController@index');
 
